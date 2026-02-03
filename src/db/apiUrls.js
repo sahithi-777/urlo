e2a78d35f3d32fa6adf446cbb7cbcc6f2e19b109
sahithi-777 <rsahithi231@gmail.com>
2026-02-03 17:40:46 +0530

    Redirect link issue fixed

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -30,20 +30,7 @@ export async function getUrl({id, user_id}) {
   return data;
 }
 
-// export async function getLongUrl(id) {
-//   let {data: shortLinkData, error: shortLinkError} = await supabase
-//     .from("urls")
-//     .select("id, original_url")
-//     .or(`short_url.eq.${id},custom_url.eq.${id}`)
-//     .single();
 
-//   if (shortLinkError && shortLinkError.code !== "PGRST116") {
-//     console.error("Error fetching short link:", shortLinkError);
-//     return;
-//   }
-
-//   return shortLinkData;
-// }
 
 export async function getLongUrl(id) {
   const normalized = id?.toLowerCase();
